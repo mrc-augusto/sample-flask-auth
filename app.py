e99a4f5a1e38a46df2bfe7a9b2f5d276cc1c4e9c
Maurício Augusto <78917687+mrc-augusto@users.noreply.github.com>
2025-07-01 14:30:59 -0300

    Logout route and method created

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, jsonify
 from database import db
 from models.user import User
-from flask_login import LoginManager, login_user, current_user
+from flask_login import LoginManager, login_user, current_user, logout_user, login_required
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'your_secret_key'
@@ -30,6 +30,14 @@ def login():
       print(current_user.is_authenticated)
       return jsonify({'message': 'Autenticação realizada com sucesso'})
   return jsonify({'message': 'Usuário ou senha inválidos'}), 400
+
+@app.route('/logout', methods=['GET'])
+@login_required
+def logout():
+  logout_user()
+  return jsonify({'message': 'Logout realizado com sucesso'})
+
+
 
 @app.route('/hello-world', methods=['GET'])
 def hello_world():
