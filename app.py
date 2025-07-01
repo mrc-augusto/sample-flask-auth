78882386af01d5a46f81dd53ffa1aa60fdeabd02
Maurício Augusto <78917687+mrc-augusto@users.noreply.github.com>
2025-07-01 15:46:28 -0300

    Update route and method created

--- app.py
+++ app.py
@@ -37,6 +37,37 @@ def logout():
   logout_user()
   return jsonify({'message': 'Logout realizado com sucesso'})
 
+@app.route('/user', methods=['POST'])
+def create_user():
+  data = request.json
+  username = data.get('username')
+  password = data.get('password')
+
+  if username and password:
+    # create user
+    user = User(username=username, password=password)
+    db.session.add(user)
+    db.session.commit()
+    return jsonify({'message': 'Usuário criado com sucesso'})
+  return jsonify({'message': 'Dados do usuário inválidos'}), 400
+
+@app.route('/user/<int:id_user>', methods=['GET'])
+@login_required
+def read_user(id_user):
+  user = User.query.get(id_user)
+  if user:
+    return jsonify({'username': user.username})
+  return jsonify({'message': 'Usuário não encontrado'}), 404
+
+@app.route('/user/<int:id_user>', methods=['PUT'])
+@login_required
+def update_user(id_user):
+  data = request.json
+  user = User.query.get(id_user)
+  if user and data.get('password'):
+    user.passowrd = data.get('password')
+    return jsonify({'message': f'Usuário {id_user} atualizado com sucesso'})
+  return jsonify({'message': 'Usuário não encontrado'}), 404
 
 
 @app.route('/hello-world', methods=['GET'])
