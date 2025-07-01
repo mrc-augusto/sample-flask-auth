3b3ff571aaa31bdd9c08c08f231d73ecafa6532b
Maurício Augusto <78917687+mrc-augusto@users.noreply.github.com>
2025-07-01 16:58:50 -0300

    DELETE route && method finished

--- app.py
+++ app.py
@@ -65,11 +65,20 @@ def update_user(id_user):
   data = request.json
   user = User.query.get(id_user)
   if user and data.get('password'):
-    user.passowrd = data.get('password')
+    user.password = data.get('password')
     db.session.commit()
     return jsonify({'message': f'Usuário {id_user} atualizado com sucesso'})
   return jsonify({'message': 'Usuário não encontrado'}), 404
 
+@app.route('/user/<int:id_user>', methods=['DELETE'])
+@login_required
+def delete_user(id_user):
+  user  = User.query.get(id_user)
+  if user:
+    db.session.delete(user)
+    db.session.commit()
+    return jsonify({'message': f'Usuário {id_user} removido com sucesso'})
+  return jsonify({'message': 'Usuário não encontrado'}), 404 
 
 @app.route('/hello-world', methods=['GET'])
 def hello_world():
